73e8483fe9cf9f7a84e52938d211e8ae340ab01f
pieman492 <rauljrrauljr@gmail.com>
2024-08-15 17:47:47 -0500

    Switch beds to sleeping bags, sleeping bags set spawn

--- kubejs/server_scripts/BedSpawn.js
+++ kubejs/server_scripts/BedSpawn.js
@@ -1,12 +1,15 @@
 BlockEvents.rightClicked(event => {
-	if (event.block.hasTag('minecraft:beds')) {
+	if (event.block.hasTag('minecraft:beds') || event.block.hasTag('comforts:sleeping_bags')) {
 		if(!throttle(event.player, 60)) { 
 			let name = event.player.username + ''
 			let pos = Math.floor(event.player.x) + ' ' + Math.floor(event.player.y) + ' ' + Math.floor(event.player.z)
 			Utils.server.scheduleInTicks(1, callback => {
 				Utils.server.runCommandSilent('execute as ' + name + ' run spawnpoint @s ' + pos)
-			Utils.server.runCommandSilent('tellraw ' + name + ' "Spawnpoint will persist even after removing the bed."')
 			})
+			Utils.server.runCommandSilent('tellraw ' + name + ' "Spawnpoint will persist even after removing the bed."')	
+			if (event.block.hasTag('comforts:sleeping_bags')) {
+				Utils.server.runCommandSilent('tellraw ' + name +  ' "Respawn point set"')
+			}
 		}
 	}
 })
