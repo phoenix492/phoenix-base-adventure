485f587fe9088c5f98cafe30f2999fba988825a6
pieman492 <rauljrrauljr@gmail.com>
2024-08-15 14:46:46 -0500

    increase throttle to 3 seconds

--- kubejs/server_scripts/BedSpawn.js
+++ kubejs/server_scripts/BedSpawn.js
@@ -1,6 +1,6 @@
 BlockEvents.rightClicked(event => {
 	if (event.block.hasTag('minecraft:beds')) {
-		if(!throttle(event.player, 40)) { 
+		if(!throttle(event.player, 60)) { 
 			let name = event.player.username + ''
 			let pos = Math.floor(event.player.x) + ' ' + Math.floor(event.player.y) + ' ' + Math.floor(event.player.z)
 			Utils.server.scheduleInTicks(1, callback => {
